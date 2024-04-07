28fd333ee313d88b48fbd23819779eb54216591b
jobscale <jobscale@users.noreply.github.com>
2024-04-07 21:39:57 +0900

    docker-entry updated

--- docker-entry.sh
+++ docker-entry.sh
@@ -1,12 +1,27 @@
-sudo mosquitto -v -d -c /etc/mosquitto/conf.d/listen.conf
+#!/usr/bin/env bash
+set -eu
 
-sleep 0.01
-nc -vz -w 1 127.0.0.1 1883
-nc -vz -w 1 127.0.0.1 8883
-nc -vz -w 1 127.0.0.1 12470
+checkStatus() {
+  while true
+  do
+    sleep 0.2
+    echo -n ' check'
+    nc -vz -w 1 127.0.0.1 1883 2>&1 || continue
+    nc -vz -w 1 127.0.0.1 8883 2>&1 || continue
+    nc -vz -w 1 127.0.0.1 12470 2>&1 || continue
+    break
+  done
+}
 
-mosquitto_pub -d -h 127.0.0.1 -p 8883 \
---cafile /etc/mosquitto/certs/ca.crt \
--t "topic/start" -m "hello world"
+main() {
+  sudo mosquitto -v -d -c /etc/mosquitto/conf.d/listen.conf
 
-npm start
+  checkStatus
+
+  mosquitto_pub -d -h 127.0.0.1 -p 1883 \
+  -t "topic/start" -m "hello world"
+
+  npm start
+}
+
+main
